6f553167e78d3a5eb14efde44a3f0ecf9d64cfa3
TomassSimko <simko.t@email.cz>
2022-09-27 16:48:31 +0200

    needs proper fixes

--- src/auth/base/auth/gui/ProfileController.java
+++ src/auth/base/auth/gui/ProfileController.java
@@ -1,25 +1,35 @@
 package auth.base.auth.gui;
 
 import auth.base.db.DbUtils;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProfileController implements Initializable {
-    public Button btn_logout;
+    public Button delete_user;
+    public Button update_psw;
+    public PasswordField user_new_psw;
+    @FXML
+    private Button btn_logout;
+    @FXML
+    private Button put_changes;
+    @FXML
+    private Label full_name;
     @FXML
     private TextField user_id;
     @FXML
     private TextField user_email;
     @FXML
     private PasswordField user_psw;
+    @FXML
+    private TextField first_name;
+    @FXML
+    private TextField last_name;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -28,14 +38,28 @@ public class ProfileController implements Initializable {
                 DbUtils.changeScene(event,
                         "/MainWindow.fxml",
                         null,
+                        null,
+                        null,
+                        null,
                         null));
+        put_changes.setOnAction(event ->
+                DbUtils.updateUser(event,user_id.getText(),user_email.getText(),first_name.getText(),last_name.getText()));
 
+        delete_user.setOnAction(event ->
+                DbUtils.deleteUser(event,user_id.getText()));
+       update_psw.setOnAction(event ->
+                DbUtils.updatePsw(event,user_id.getText(),user_psw.getText(),user_new_psw.getText()));
     }
 
     @FXML
-    public void setUser(String email,String password){
-       //  user_id.setText(id);
+    public void setUser(String id,String email,String password,String firstName,String lastName){
+        String fullName = firstName + " " + lastName;
+        user_id.setText(id);
         user_email.setText(email);
         user_psw.setText(password);
+        first_name.setText(firstName);
+        last_name.setText(lastName);
+        full_name.setText(fullName);
+
     }
 }
--- src/auth/base/db/DbUtils.java
+++ src/auth/base/db/DbUtils.java
@@ -8,8 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-
-// import java.awt.event.ActionEvent;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,7 +17,7 @@ import java.sql.SQLException;
 import java.util.Objects;
 
 public class DbUtils {
-    public static void changeScene(ActionEvent event,String fxmlFile,String email,String password){
+    public static void changeScene(ActionEvent event,String fxmlFile,String id,String email,String password,String firstName,String lastName){
         Parent root = null;
 
         if(email != null && password != null){
@@ -26,7 +25,7 @@ public class DbUtils {
                 FXMLLoader loader = new FXMLLoader(DbUtils.class.getResource(fxmlFile));
                 root = loader.load();
                 ProfileController pc = loader.getController();
-                pc.setUser(email,password);
+                pc.setUser(id,email,password,firstName,lastName);
             }catch(IOException e){
                 e.printStackTrace();
             }
@@ -39,7 +38,7 @@ public class DbUtils {
         }
 
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setTitle("Welcome");
+        stage.setTitle("Welcome back");
         stage.setScene(new Scene(root));
         stage.show();
     }
@@ -63,10 +62,13 @@ public class DbUtils {
                  alert.show();
              }else{
                 while(set.next()){
+                    String retrievedId = set.getString("id");
                     String retrievedEmail = set.getString("email");
                     String retrievedPassword = set.getString("password");
+                    String retrievedFirstName = set.getString("first_name");
+                    String retrievedLastName = set.getString("last_name");
                     if(retrievedEmail.equals(email) && retrievedPassword.equals(password)){
-                        changeScene(event,"/Profile.fxml",retrievedEmail,retrievedPassword);
+                        changeScene(event,"/Profile.fxml",retrievedId,retrievedEmail,retrievedPassword,retrievedFirstName,retrievedLastName);
                     } else {
                         System.out.print("Psw do not match");
                         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -86,6 +88,157 @@ public class DbUtils {
                     e.printStackTrace();
                 }
             }
+            if(uExist != null){
+                try {
+                    uExist.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // TODO: sort event and sort refreshing of GUI
+    public static void updateUser(ActionEvent event, String id, String email, String firstName, String lastName) {
+        DbConnection cnn = new DbConnection();
+        Connection connection = null;
+        PreparedStatement uExist = null;
+        int set;
+        try{
+            connection = cnn.getConnection();
+            uExist = connection.prepareStatement("UPDATE user SET email = ?,first_name = ?,last_name = ? WHERE id = ?");
+            uExist.setString(1,email);
+            uExist.setString(2,firstName);
+            uExist.setString(3,lastName);
+            uExist.setString(4,id);
+
+            set = uExist.executeUpdate();
+
+            if(set > 0) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("Successfully updated user");
+                alert.show();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Could not update user !");
+                alert.show();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        } finally {
+            if(uExist != null){
+                try {
+                    uExist.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+    public static void deleteUser(ActionEvent event, String id) {
+        DbConnection cnn = new DbConnection();
+        Connection connection = null;
+        PreparedStatement uExist = null;
+        boolean set;
+        try{
+            connection = cnn.getConnection();
+            uExist = connection.prepareStatement("DELETE FROM user WHERE id = ?");
+            uExist.setString(1,id);
+
+            set = uExist.execute();
+
+            if(!set) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("Successfully deleted user");
+                alert.show();
+                // TODO: Do better even handeling
+                changeScene(event,"/MainWindow.fxml",null,null,null,null,null);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Could not delete user!");
+                alert.show();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        } finally {
+            if(uExist != null){
+                try {
+                    uExist.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    // TODO: Check password against db hash and hash again
+    public static void updatePsw(ActionEvent event,String id, String oldPsw, String newPsw) {
+         Argon2PasswordEncoder encoder = new Argon2PasswordEncoder(32,64,1,15*1024,2);
+
+        if(oldPsw == null || newPsw == null){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Neither old password or new password cannot be empty !");
+            alert.show();
+        }
+        // test one
+        var encodedPassword = encoder.encode(oldPsw);
+        System.out.println(encodedPassword);
+
+        var validPassword = encoder.matches(oldPsw, encodedPassword);
+        System.out.println(validPassword);
+
+        DbConnection cnn = new DbConnection();
+        Connection connection = null;
+        PreparedStatement uExist = null;
+        int set;
+
+//        UPDATE
+//                app_user
+//        SET
+//                passwordHash = @passwordHash
+//                WHERE id = @id;
+        try{
+            connection = cnn.getConnection();
+            uExist = connection.prepareStatement("UPDATE user SET password = ? WHERE id = ?");
+            uExist.setString(1,newPsw);
+            uExist.setString(2,id);
+            set = uExist.executeUpdate();
+
+            if(set > 0) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("Successfully updated password");
+                alert.show();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Could not update password !");
+                alert.show();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        } finally {
             if(uExist != null){
                 try {
                     uExist.close();

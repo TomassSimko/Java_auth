879201f0473ca37b9cc76475fa9b487e7e4a47a7
TomassSimko <simko.t@email.cz>
2022-10-27 11:52:18 +0200

    search heÃlper added comments

--- src/bll/utitls/validations/SearchHelper.java
+++ src/bll/utitls/validations/SearchHelper.java
@@ -3,6 +3,10 @@ package bll.utitls.validations;
 import be.User;
 import java.util.List;
 public class SearchHelper {
+    /*
+    / For now just filters base with given query that is email string type
+    / @return filtered list of users;
+     */
     public List<User> search(List<User> base, String query) {
         return !query.isEmpty() ?
                 base.stream().filter((user) ->
